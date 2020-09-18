dbd54b0192325130b82d296d925bfdee7103c08c
Saniya Suhail <suhailsaniya003@gmail.com>
2020-09-18 18:04:40 +0530

    footer file

---
+++ footer.php
@@ -0,0 +1,10 @@
+
+<footer class="page-footer font-small black">
+
+  
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+  </div>
+  
+
+</footer>
